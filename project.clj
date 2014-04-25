b88e8af8b041724348fa1da68b1aa4eaf5964e3e
Magnar Sveen <magnars@gmail.com>
2014-04-25 08:25:39 +0200

    Add URL to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject stasis "1.0.0"
   :description "A library of tools for creating static websites."
+  :url "http://github.com/magnars/stasis"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
