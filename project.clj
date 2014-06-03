c0a2e96d566fec6d5d9ff02a8dd65d0c5838135d
Magnar Sveen <magnars@gmail.com>
2014-06-03 13:25:50 +0200

    Release 2.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject stasis "2.1.0"
+(defproject stasis "2.1.1"
   :description "A library of tools for creating static websites."
   :url "http://github.com/magnars/stasis"
   :license {:name "Eclipse Public License"
