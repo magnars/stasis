5b252d1ed84d7fe71f6e0bb32d234d12deef4128
Stephen Starkey <stephen@calmabiding.me>
2019-08-08 11:37:23 -0400

    Added unit test for binary support
    Noticed some dependencies were out of date, so added ancient plugin. Then used that to upgrade them.

--- project.clj
+++ project.clj
@@ -3,10 +3,12 @@
   :url "http://github.com/magnars/stasis"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[ring/ring-codec "1.1.1"]
+  :dependencies [[ring/ring-codec "1.1.2"]
                  [narkisr/clansi "1.2.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]
-                                  [midje "1.9.4"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
+                                  [digest "1.4.9"]
+                                  [midje "1.9.9"]
                                   [test-with-files "0.1.1"]]
-                   :plugins [[lein-midje "3.2.1"]]
+                   :plugins [[lein-ancient "0.6.15"]
+                             [lein-midje "3.2.1"]]
                    :source-paths ["dev"]}})
