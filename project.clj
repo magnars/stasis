1d02227ca6560331ab7962fe15b3391d8ea03997
John Swanson <john.d.swanson@gmail.com>
2023-05-31 09:38:39 -0700

    update `ring/ring-codec`
    Thanks so much for building stasis, I've really enjoyed using it!
    
    I made this change locally so that I could use stasis from babashka,
    which makes for a really nice flow because of the minimal startup time.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/magnars/stasis"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[ring/ring-codec "1.1.2"]
+  :dependencies [[ring/ring-codec "1.2.0"]
                  [narkisr/clansi "1.2.0"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
                                   [digest "1.4.9"]
